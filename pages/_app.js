d943bc8148f6aa1314b4bc31567bf3ff73199eb4
Alefiya Abbas <alefiyar02@gmail.com>
2023-06-23 15:20:00 +0530

    Integrated custom fonts

--- pages/_app.js
+++ pages/_app.js
@@ -1,15 +1,20 @@
 import '@/styles/globals.css'
 import localFont from 'next/font/local'
 
+const cabinet = localFont({
+	src: "../public/fonts/Cabinet/CabinetGrotesk-Variable.woff2",
+	variable: "--font-cabinet",
+})
 
-const cabinet = localFont({ src: "../public/fonts/Cabinet/CabinetGrotesk-Light.woff2" })
+const britney = localFont({
+	src: "../public/fonts/Britney/Britney-Variable.woff2",
+	variable: "--font-britney",
+})
 
 export default function App({ Component, pageProps }) {
-  return (
-    <main className={` ${cabinet.className}`}>
-      <Component {...pageProps} />
-    </main>
-
-
-  )
+	return (
+		<main className={`${britney.variable} ${cabinet.variable}`}>
+			<Component {...pageProps} />
+		</main>
+	)
 }
