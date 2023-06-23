bb4fb2ff9ef134c035515505dd3b66033947b5bc
Alefiya Abbas <alefiyar02@gmail.com>
2023-06-23 15:20:24 +0530

    Updated Navbar

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,30 +1,92 @@
-import React from 'react';
-import Link from 'next/link';
-import localFont from 'next/font/local'
+// import React from 'react';
+// import Link from 'next/link';
+// import localFont from 'next/font/local'
 
-const britney = localFont({ src: "../public/fonts/Britney/Britney-Variable.woff2" })
-export const Navbar = () => {
+// export const Navbar = () => {
+//     return (
+//         <header>
+//             <nav className="flex justify-around py-2 bg-slate-800 text-slate-200 items-center">
+//                 <div id="logo">
+//                     <Link href="/" className='text-3xl font-britney'>  <span className='flex gap-[0.1px]'><span>Alef</span>iya Abbas </span> </Link>
+//                 </div>
+//                 <div>
+//                 </div>
+//                 <ul className="flex gap-10 justify-around align-middle font-cabinet text-xl">
+//                     <li>
+//                         <Link href="/about"> About </Link>
+//                     </li>
+//                     <li>
+//                         <Link href="/experience"> Experiences </Link>
+//                     </li>
+//                     <li>
+//                         <Link href="/projects"> Projects </Link>
+//                     </li>
+//                 </ul>
+//             </nav>
+//         </header>
+//     );
+// };
+
+import { FaBars, FaTimes } from 'react-icons/fa'
+import { useEffect, useState } from 'react'
+
+export default function Navbar() {
+    const links = ['Home', 'About', 'Experience', 'Contact']
+    const [Nav, setNav] = useState(false)
+
+    useEffect(() => {
+        let lastScrollTop = 0
+        let navbar = document.getElementById('nav')
+        window.addEventListener('scroll', () => {
+            let scrollTop = window.scrollY || document.documentElement.scrollTop
+            if (scrollTop > lastScrollTop) {
+                navbar.style.top = '-100px'
+            }
+            else {
+                navbar.style.top = '0'
+            }
+            lastScrollTop = scrollTop
+        })
+    })
+
+    const HorizontalNavList = ({ link }) => {
+        return (
+            <li className="px-4 cursor-pointer capitalize font-medium text-slate-200 hover:scale-105 duration-200">
+                {link}
+            </li>
+        )
+    }
+    const VerticalNavList = ({ link }) => {
+        return (
+            <li className="px-4 capitalize py-6 text-4xl cursor-pointer">{link}</li>
+        )
+    }
+
     return (
-        <header>
-            <nav className="flex justify-around py-2 bg-slate-800 text-slate-200 items-center">
-                <div id="logo" className={britney.className}>
-                    <Link href="/" className='text-4xl'>  <span className='flex gap-[0.1px]'><span>Alef</span>iya Abbas </span> </Link>
-                </div>
-                <div>
-                </div>
-                <ul className="flex gap-10 justify-around align-middle">
-                    <li>
-                        <Link href="/about"> About </Link>
-                    </li>
-                    <li>
-                        <Link href="/experience"> Experiences </Link>
-                    </li>
-                    <li>
-                        <Link href="/projects"> Projects </Link>
-                    </li>
+        <nav id="nav" className='fixed w-full duration-700'>
+            <div className="flex justify-between items-center w-full h-12  bg-slate-800 text-slate-200 px-4">
+                <h1 className="text-3xl ml-2 font-britney"> <span className='flex gap-[0.1px]'><span>Alef</span>iya Abbas </span></h1>
+                <ul className="hidden md:flex font-cabinet text-lg">
+                    {links.map((link, index) => {
+                        return <HorizontalNavList key={index} link={link} />
+                    })}
                 </ul>
-            </nav>
-        </header>
-    );
-};
-
+                <div
+                    onClick={() => {
+                        setNav(!Nav)
+                    }}
+                    className="cursor-pointer md:hidden text-slate-200 pr-4 z-10"
+                >
+                    {Nav ? <FaTimes size={20} /> : <FaBars size={20} />}
+                </div>
+                {Nav && (
+                    <ul className="flex flex-col justify-center items-center fixed top-0 left-0 w-full h-full bg-gradient-to-b from-slate-900 to-slate-500 text-slate-200 font-cabinet">
+                        {links.map((link, index) => {
+                            return <VerticalNavList key={index} link={link} />
+                        })}
+                    </ul>
+                )}
+            </div>
+        </nav>
+    )
+}
--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,12 @@
 import Image from 'next/image'
-import { Inter } from 'next/font/google'
-import { Navbar } from '@/components/Navbar'
-
-const inter = Inter({ subsets: ['latin'] })
+import Navbar from '@/components/Navbar'
+import localFont from 'next/font/local'
 
 export default function Home() {
-  return (
-  <>
-    <Navbar/>
-  </>
-  )
+	return (
+		<main>
+			<Navbar />
+
+		</main>
+	)
 }
