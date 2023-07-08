de2447d7b3db1a4c01208492f0ad6b06ee74cac0
Alefiya Abbas <alefiyar02@gmail.com>
2023-07-08 21:44:35 +0530

    Updates

--- components/About.js
+++ components/About.js
@@ -2,6 +2,7 @@ import { motion } from 'framer-motion'
 import Alef from "../public/images/alef.png"
 import Image from 'next/image'
 
+
 export default function About() {
   const Header = () => {
     return (
@@ -12,41 +13,64 @@ export default function About() {
           transition={{ duration: 1, delay: 4.5 }}
           className='bg-zinc-800 w-1/2 ml-auto text-slate-200 h-10 mt-4 text-2xl md:h-16 md:w-1/4 md:mt-6 md:text-3xl px-4'
         >
-          <span className='self-center leading-10 md:leading-[64px]'>About Me</span>
+          <span className='self-center leading-10 md:leading-[64px]  hover:text-teal-500'>About Me</span>
         </motion.div>
       </div>
     )
   }
-
   return (
     <div className='bg-zinc-800 h-min-screen flex justify-center items-center font-cabinet py-10'>
-      <div className='bg-slate-200 h-auto w-[90%] py-6'>
+      <div className='bg-slate-200 h-auto w-[90%] py-8'>
         <Header />
-        <div className='flex flex-col w-11/12 items-center justify-center gap-12 mx-auto md:flex-row'>
-          <motion.div
-            initial={{ x: -1000 }}
-            animate={{ x: 0 }}
-            transition={{ duration: 2, delay: 5.5 }}
-            className=' bg-teal-400 w-[40%] md:w-[98%] mt-10 mx-8 md:mt-0'>
-            <Image src={Alef} className='relative -translate-x-4 -translate-y-4 md:-translate-x-6 md:-translate-y-6 ' />
-          </motion.div>
-          <div style={{ overflow: 'hidden' }}>
-            <motion.ul
-              initial={{ x: 1000 }}
+        <section className='max-w-screen-xl mx-auto py-2'>
+          <div className='container mx-auto flex md:flex-row-reverse flex-col-reverse items-center'>
+            <div className='lg:flex-grow md:w-1/2 flex flex-col md:items-start md:text-left items-center text-center'>
+              <div style={{ overflow: 'hidden' }}>
+                <motion.ul
+                  initial={{ x: 1000 }}
+                  animate={{ x: 0 }}
+                  transition={{ duration: 1, delay: 5.5 }}
+                  className="flex flex-col gap-4 text-zinc-800 text-xl md:text-3xl text-justify px-10  md:leading-10">
+                  <li className="text-center md:text-left "> Hello World, I&apos;m <b>Alefiya!</b> </li>
+                  <li> An aspiring engineer in <b>Computer Science</b> with a deep passion for <b>Artificial Intelligence and Machine Learning</b>.</li>
+                  <li>
+                    Throughout my journey, I have acquired strong expertise in <b>Python, Java, SQL, React, and Next.js</b>, which have been instrumental in my exploration and development of innovative solutions.
+                    Aside from my technical proficiency, I value <b>collaboration and teamwork</b>.
+                  </li>
+                  <li> Explore my portfolio to see my work. Let&apos;s <b>connect and create</b> something extraordinary!</li>
+                </motion.ul>
+              </div>
+            </div>
+            {/* <div className='lg:max-w-md md:w-1/2 w-5/6 flex items-center justify-center py-6'>
+            <motion.div
+              initial={{ x: -1000 }}
               animate={{ x: 0 }}
-              transition={{ duration: 1, delay: 5.5 }}
-              className="flex flex-col gap-4 text-zinc-800 text-xl md:text-3xl text-justify px-8  md:p-10 md:leading-10">
-              <li className="text-center md:text-left text-2xl md:text-3xl"> Hello World, I&apos;m <b>Alefiya!</b> </li>
-              <li> An aspiring engineer in <b>Computer Science</b> with a deep passion for <b>Artificial Intelligence and Machine Learning</b>.</li>
-              <li>
-                Throughout my journey, I have acquired strong expertise in <b>Python, Java, SQL, React, and Next.js</b>, which have been instrumental in my exploration and development of innovative solutions.
-                Aside from my technical proficiency, I value <b>collaboration and teamwork</b>.
-              </li>
-              <li> Explore my portfolio to see my work. Let&apos;s <b>connect and create</b> something extraordinary!</li>
-            </motion.ul>
+              transition={{ duration: 2, delay: 5.5 }}
+              className='border-4 border-teal-400 w-1/3 md:w-2/3 mt-10 mx-8 md:mt-0'>
+              <Image className="object-cover relative -translate-x-4 -translate-y-4"  src={Alef} alt="Alef" />
+            </motion.div>
+          </div> */}
+            <div class="cardContainer">
+              <div class="profileDiv">
+                <Image className="object-cover" src={Alef} alt="Alef" />
+              </div>
+              <div class="infoDiv">
+                <div class="nameDiv">
+                  <p class="name">Alefiya Abbas</p>
+                </div>
+                <div class="socialDiv">
+                  <a href="https://github.com/AlefiyaAbbas"><svg viewBox="0 0 496 500" class="socials github"><path d="M165.9 397.4c0 2-2.3 3.6-5.2 3.6-3.3.3-5.6-1.3-5.6-3.6 0-2 2.3-3.6 5.2-3.6 3-.3 5.6 1.3 5.6 3.6zm-31.1-4.5c-.7 2 1.3 4.3 4.3 4.9 2.6 1 5.6 0 6.2-2s-1.3-4.3-4.3-5.2c-2.6-.7-5.5.3-6.2 2.3zm44.2-1.7c-2.9.7-4.9 2.6-4.6 4.9.3 2 2.9 3.3 5.9 2.6 2.9-.7 4.9-2.6 4.6-4.6-.3-1.9-3-3.2-5.9-2.9zM244.8 8C106.1 8 0 113.3 0 252c0 110.9 69.8 205.8 169.5 239.2 12.8 2.3 17.3-5.6 17.3-12.1 0-6.2-.3-40.4-.3-61.4 0 0-70 15-84.7-29.8 0 0-11.4-29.1-27.8-36.6 0 0-22.9-15.7 1.6-15.4 0 0 24.9 2 38.6 25.8 21.9 38.6 58.6 27.5 72.9 20.9 2.3-16 8.8-27.1 16-33.7-55.9-6.2-112.3-14.3-112.3-110.5 0-27.5 7.6-41.3 23.6-58.9-2.6-6.5-11.1-33.3 2.6-67.9 20.9-6.5 69 27 69 27 20-5.6 41.5-8.5 62.8-8.5s42.8 2.9 62.8 8.5c0 0 48.1-33.6 69-27 13.7 34.7 5.2 61.4 2.6 67.9 16 17.7 25.8 31.5 25.8 58.9 0 96.5-58.9 104.2-114.8 110.5 9.2 7.9 17 22.9 17 46.4 0 33.7-.3 75.4-.3 83.6 0 6.5 4.6 14.4 17.3 12.1C428.2 457.8 496 362.9 496 252 496 113.3 383.5 8 244.8 8zM97.2 352.9c-1.3 1-1 3.3.7 5.2 1.6 1.6 3.9 2.3 5.2 1 1.3-1 1-3.3-.7-5.2-1.6-1.6-3.9-2.3-5.2-1zm-10.8-8.1c-.7 1.3.3 2.9 2.3 3.9 1.6 1 3.6.7 4.3-.7.7-1.3-.3-2.9-2.3-3.9-2-.6-3.6-.3-4.3.7zm32.4 35.6c-1.6 1.3-1 4.3 1.3 6.2 2.3 2.3 5.2 2.6 6.5 1 1.3-1.3.7-4.3-1.3-6.2-2.2-2.3-5.2-2.6-6.5-1zm-11.4-14.7c-1.6 1-1.6 3.6 0 5.9 1.6 2.3 4.3 3.3 5.6 2.3 1.6-1.3 1.6-3.9 0-6.2-1.4-2.3-4-3.3-5.6-2z"></path></svg></a>
+                  <a href="https://www.linkedin.com/in/alefiya-rampurawala-30851a204/"><svg viewBox="0 0 448 512" class="socials linkdin"><path d="M100.28 448H7.4V148.9h92.88zM53.79 108.1C24.09 108.1 0 83.5 0 53.8a53.79 53.79 0 0 1 107.58 0c0 29.7-24.1 54.3-53.79 54.3zM447.9 448h-92.68V302.4c0-34.7-.7-79.2-48.29-79.2-48.29 0-55.69 37.7-55.69 76.7V448h-92.78V148.9h89.08v40.8h1.3c12.4-23.5 42.69-48.3 87.88-48.3 94 0 111.28 61.9 111.28 142.3V448z"></path></svg></a>
+                  <a href="#insta"><svg class="socials instagram" viewBox="0 0 448 512"><path d="M224.1 141c-63.6 0-114.9 51.3-114.9 114.9s51.3 114.9 114.9 114.9S339 319.5 339 255.9 287.7 141 224.1 141zm0 189.6c-41.1 0-74.7-33.5-74.7-74.7s33.5-74.7 74.7-74.7 74.7 33.5 74.7 74.7-33.6 74.7-74.7 74.7zm146.4-194.3c0 14.9-12 26.8-26.8 26.8-14.9 0-26.8-12-26.8-26.8s12-26.8 26.8-26.8 26.8 12 26.8 26.8zm76.1 27.2c-1.7-35.9-9.9-67.7-36.2-93.9-26.2-26.2-58-34.4-93.9-36.2-37-2.1-147.9-2.1-184.9 0-35.8 1.7-67.6 9.9-93.9 36.1s-34.4 58-36.2 93.9c-2.1 37-2.1 147.9 0 184.9 1.7 35.9 9.9 67.7 36.2 93.9s58 34.4 93.9 36.2c37 2.1 147.9 2.1 184.9 0 35.9-1.7 67.7-9.9 93.9-36.2 26.2-26.2 34.4-58 36.2-93.9 2.1-37 2.1-147.8 0-184.8zM398.8 388c-7.8 19.6-22.9 34.7-42.6 42.6-29.5 11.7-99.5 9-132.1 9s-102.7 2.6-132.1-9c-19.6-7.8-34.7-22.9-42.6-42.6-11.7-29.5-9-99.5-9-132.1s-2.6-102.7 9-132.1c7.8-19.6 22.9-34.7 42.6-42.6 29.5-11.7 99.5-9 132.1-9s102.7-2.6 132.1 9c19.6 7.8 34.7 22.9 42.6 42.6 11.7 29.5 9 99.5 9 132.1s2.7 102.7-9 132.1z"></path></svg></a>
+                </div>
+              </div>
+            </div>
+
           </div>
-        </div>
+        </section>
       </div>
     </div>
+
+
   )
 }
--- components/Navbar.js
+++ components/Navbar.js
@@ -1,157 +1,133 @@
-import { useEffect, useState } from 'react'
-import { motion, AnimatePresence } from 'framer-motion'
+import { useEffect, useState } from 'react';
+import { motion, AnimatePresence } from 'framer-motion';
 
 export default function Navbar() {
-  const links = ['Home', 'About', 'Experience', 'Contact']
-  const [mobileNav, setMobileNav] = useState(false)
+  const links = ['Home', 'About', 'Experience', 'Contact'];
+  const [mobileNav, setMobileNav] = useState(false);
 
   useEffect(() => {
-    let lastScrollTop = 0
-    let navbar = document.getElementById('nav')
+    let lastScrollTop = 0;
+    let navbar = document.getElementById('nav');
     window.addEventListener('scroll', () => {
-      let scrollTop = window.scrollY || document.documentElement.scrollTop
+      let scrollTop = window.scrollY || document.documentElement.scrollTop;
       if (scrollTop > lastScrollTop) {
-        navbar.style.top = '-100px'
+        navbar.style.top = '-100px';
       } else {
-        navbar.style.top = '0'
+        navbar.style.top = '0';
       }
-      lastScrollTop = scrollTop
-    })
-  }, [])
+      lastScrollTop = scrollTop;
+    });
+  }, []);
 
   useEffect(() => {
-    let navbar = document.getElementById('nav')
-    navbar.style.top = '-100px'
+    let navbar = document.getElementById('nav');
+    navbar.style.top = '-100px';
     setTimeout(() => {
-      navbar.style.top = '0'
-    }, 3000)
-  }, [])
+      navbar.style.top = '0';
+    }, 3000);
+  }, []);
 
   const HorizontalNavList = ({ link }) => {
     return (
-      <li className='px-4 cursor-pointer capitalize font-medium  text-slate-200 hover:scale-105 duration-200'>
+      <li className="px-4 cursor-pointer capitalize font-medium  text-slate-200 hover:scale-105 duration-200 hover:text-teal-300">
         {link}
       </li>
-    )
-  }
+    );
+  };
   const VerticalNavList = ({ link }) => {
     return (
       <motion.li
-        className='px-4 capitalize py-6 text-4xl cursor-pointer'
+        className="px-4 capitalize py-6 text-4xl cursor-pointer hover:text-teal-300"
         variants={{
           open: {
             y: 0,
-            opacity: 1
+            opacity: 1,
           },
           closed: {
             y: '45%',
-            opacity: 0
-          }
+            opacity: 0,
+          },
         }}
       >
         {link}
       </motion.li>
-    )
-  }
-
-  const topVariants = {
-    open: { rotate: 45, y: 7, originX: '16px', originY: '10px' },
-    closed: { rotate: 0, y: 0, originX: 0, originY: 0 }
-  }
-
-  const centerVariants = {
-    open: { opacity: 0 },
-    closed: { opacity: 1 }
-  }
-
-  const bottomVariants = {
-    open: { rotate: -45, y: -5, originX: '16px', originY: '22px' },
-    closed: { rotate: 0, y: 0, originX: 0, originY: 0 }
-  }
+    );
+  };
 
   return (
-    <nav id='nav' className='fixed w-full duration-200 font-cabinet z-50'>
+    <nav id="nav" className="fixed w-full duration-200 font-cabinet">
       <div className="flex justify-end items-center w-full h-12 bg-zinc-800 text-slate-200 px-4">
         <ul className="hidden md:flex font-cabinet text-lg p-2">
           {links.map((link, index) => {
-            return <HorizontalNavList key={index} link={link} />
+            return <HorizontalNavList key={index} link={link} />;
           })}
         </ul>
         <div
           onClick={() => {
-            setMobileNav(!mobileNav)
+            setMobileNav(!mobileNav);
           }}
-          className='cursor-pointer md:hidden text-slate-200 pr-4 z-10'
+          className="cursor-pointer md:hidden text-slate-200 pr-4 z-10"
         >
-          <svg
-            width='40'
-            height='40'
-            viewBox='0 0 32 32'
-            fill='none'
-            xmlns='http://www.w3.org/2000/svg'
+          <motion.button
+            initial='hide'
+            animate={mobileNav ? 'show' : 'hide'}
+            onClick={() => {
+              setMobileNav(!mobileNav)
+            }}
+            className='flex flex-col gap-1 relative z-10 h-8 justify-center items-center'
           >
-            <motion.rect
-              animate={mobileNav ? 'open' : 'closed'}
-              variants={topVariants}
-              transition={{ duration: 0.2 }}
-              x='6'
-              y='9'
-              width='20'
-              height='2'
-              rx='1'
-              fill='currentColor'
-            />
-            <motion.rect
-              animate={mobileNav ? 'open' : 'closed'}
-              variants={centerVariants}
-              transition={{ duration: 0.2 }}
-              x='6'
-              y='15'
-              width='20'
-              height='2'
-              rx='1'
-              fill='currentColor'
-            />
-            <motion.rect
-              animate={mobileNav ? 'open' : 'closed'}
-              variants={bottomVariants}
-              transition={{ duration: 0.2 }}
-              x='6'
-              y='21'
-              width='20'
-              height='2'
-              rx='1'
-              fill='currentColor'
-            />
-          </svg>
+            <motion.span
+              variants={{
+                hide: {
+                  rotate: 0
+                },
+                show: {
+                  rotate: 45,
+                }
+              }}
+              className='w-6 bg-slate-200 h-[1px]'
+            ></motion.span>
+            <motion.span
+              variants={{
+                hide: {
+                  rotate: 0
+                },
+                show: {
+                  rotate: -45,
+                  y: -5
+                }
+              }}
+              className='w-6 bg-slate-200 h-[1px]'
+            ></motion.span>
+          </motion.button>
         </div>
         <AnimatePresence>
           {mobileNav && (
             <motion.div
               variants={{
                 open: {
-                  y: '0%',
+                  x: '0%',
                   transition: {
                     when: 'beforeChildren',
-                    duration: 0.5
-                  }
+                    duration: 0.5,
+                  },
                 },
                 closed: {
-                  y: '-100%',
+                  x: '100%',
                   transition: {
                     when: 'afterChildren',
-                    duration: 0.5
-                  }
-                }
+                    duration: 0.5,
+                  },
+                },
               }}
-              initial='closed'
-              animate={'open'}
-              exit={'closed'}
-              className='flex flex-col justify-center items-center fixed top-0 left-0 w-screen h-full bg-gradient-to-b from-black to-stone-900 opacity-90'
+              initial="closed"
+              animate={mobileNav ? 'open' : 'closed'}
+              exit="closed"
+              className="flex flex-col justify-center items-center fixed top-0 left-0 w-screen h-full bg-gradient-to-b from-zinc-800 to-zinc-900 opacity-90"
             >
-              <ul className='text-center'>
+              <ul className="text-center">
                 {links.map((link, index) => {
-                  return <VerticalNavList key={index} link={link} />
+                  return <VerticalNavList key={index} link={link} />;
                 })}
               </ul>
             </motion.div>
@@ -159,5 +135,5 @@ export default function Navbar() {
         </AnimatePresence>
       </div>
     </nav>
-  )
+  );
 }
