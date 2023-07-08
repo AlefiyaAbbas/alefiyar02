3554476666475e6a363d8f054dabbd5cc62686e7
Alefiya Abbas <alefiyar02@gmail.com>
2023-07-08 21:45:44 +0530

    Updates

--- pages/index.js
+++ pages/index.js
@@ -1,15 +1,17 @@
 import Navbar from '@/components/Navbar'
-import Hero  from '@/components/Hero'
+import Hero from '@/components/Hero'
 import About from '@/components/About'
 import Skills from '@/components/Skills'
+import Project from '@/components/Project'
 
 export default function Home() {
 	return (
 		<main>
 			<Navbar />
 			<Hero />
-			<About/>
-			<Skills/>
+			<About />
+			<Skills />
+			<Project />
 		</main>
 	)
 }
